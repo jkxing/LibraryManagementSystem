dede57f2810eb4a42c84525c293c318059922f0a
jkxing <jkxing1210@gmail.com>
2018-05-11 18:16:32 +0800

    jkxing fix bugs

--- LibraryManagementSystem/main.cpp
+++ LibraryManagementSystem/main.cpp
@@ -1,20 +1,49 @@
 #include <QCoreApplication>
+#include <algorithm>
 #include <iostream>
+#include <cstdio>
 #include <string>
 using namespace std;
 class AbstractApp{
-    vector<User*> users;
+protected:
+    //vector<User*> users;
+    //vector<Book*> books;
+    enum registerState{
+        Success,
+        UserAlreadyExists,
+        UserNameInvalid,
+        PasswordInvalid
+    };
+#ifndef DataBase
+    map<string,string> userList;
+#endif
 public:
     virtual string getInput() = 0;
-    virtual string ShowHelpPages() = 0;
+    virtual void ShowHelpPages() = 0;
     virtual void showMessage(const string &str) = 0;
-    void addUser();
+    virtual void Register() = 0;
     virtual void exit() = 0;
+    //vector<Books*> searchByName(const string &str);
+    registerState addUser(const string &username,const string &password);
 };
-string AbstractApp::ShowHelpPages(){
-    cout<<"Haha! No such thing!"<<endl;
+AbstractApp::registerState AbstractApp::addUser(const string &username,const string &password){
+#ifndef DataBase
+    if(userList.count(username) > 0) return UserAlreadyExists;
+    else if(username.length()<5) return UserNameInvalid;
+    else if(password.length()<6) return PasswordInvalid;
+    userList[username]=password;
+    return Success;
+#endif
+    //DataBase.addUser(username,password);
 }
-
+/*template<typename T>
+vector<T*> AbstractApp::searchByName(const vector<string> &strs){
+#ifndef DataBase
+    SeacherEngine* searcher = new NameSeacher<T*>();
+    searcher->work();
+    return
+#endif
+}*/
 class ConsoleApp:public AbstractApp{
     string str;
 public:
@@ -24,6 +53,20 @@ public:
     }
     void showMessage(const string &str){
         cout<<str<<endl;
+    }
+    void Register(){
+        this->showMessage("Please input your username:");
+        string Username = getInput();
+        this->showMessage("Please input your password");
+        string Password = getInput();
+        registerState st = addUser(Username,Password);
+        if(st == Success)
+            cout<<"registered Succeed"<<endl;
+        else
+            cout<<"registered failed"<<endl;
+    }
+    void ShowHelpPages(){
+        //cout<<"Haha! No such thing!"<<endl;
     }
     void exit(){
         cout<<"Bye!"<<endl;
@@ -58,12 +101,16 @@ int main(int argc, char *argv[])
     int order;
     while((order = get_option())!=-1)
     {
-        if(order == )
-        System->showMessage(to_string(order));
+        if(order == 1)
+        {
+            System->Register();
+        }
+        else
+        {
+            System->showMessage("Unexpected order");
+        }
+        //System->showMessage(to_string(order));
     }
     System->exit();
     return 0;
-    //QCoreApplication a(argc, argv);
-
-    //return a.exec();
 }
