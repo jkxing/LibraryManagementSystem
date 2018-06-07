d3f9e050a6104ae19dea2d49de65404272394d0e
zkz <1479612681@qq.com>
2018-06-07 10:27:32 +0800

    Administrator add_book

--- LibraryManagementSystem/Administrator.h
+++ LibraryManagementSystem/Administrator.h
@@ -2,17 +2,65 @@
 #define ADMINISTRAOR_H
 
 #include "User.h"
+#include "shop.h"
+#include "searcher.h"
 
 class Administrator : public User{
 public:
     Administrator(string a, string b):User(a,b){}
     //添加书
-    void add_book(string a){//a是有关于书的信息，包括书名、作者...
-        /*if(//这个信息不存在)
-            把这个信息转换成书储存起来;
+    void add_book(){
+        string str;
+        int label;
+        bsoncxx::builder::basic::document basic_builder{};
+        cout << "请输入书名..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("书名", str));
+        cout << "请输入作者..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("作者", str));
+
+        cout << "是否有翻译人员？Y/N" << endl;
+        cin >> str;
+        if (str == "Y" || str == "y")
+        {
+            cout << "请输入译者..." << endl;
+            cin >> str;
+            basic_builder.append(kvp("书名", str));
+        }
+
+        cout << "请输入标签个数..." << endl;
+        cin >> label;
+        bsoncxx::builder::basic::document another_builder{};
+        for (int i=0; i<label; i++)
+        {
+            cout << "请输入一个标签..." << endl;
+            cin >> str;
+            stringstream stream;
+            string count;
+            stream << i;
+            stream >> count;
+            another_builder.append(kvp(count, str));
+        }
+        basic_builder.append(kvp("标签", another_builder));
+
+        cout << "请输入出版社..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("出版社", str));
+        cout << "请输入出版时间..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("出版时间", str));
+        cout << "请输入推荐语..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("推荐语", str));
+        cout << "请输入ISBN编号..." << endl;
+        cin >> str;
+        basic_builder.append(kvp("ISBN", str));
+        bsoncxx::document::value document = basic_builder.extract();
+        if(searcher::search(document) != nullptr)
+            shop::addItem(document);
         else
             cout << "你已经添加过这本书了~" << endl;
-        */
     }
     //删除书
     void omit_book(string a){//a有关于书的信息
